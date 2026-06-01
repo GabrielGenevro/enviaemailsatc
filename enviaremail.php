6f6d23ac2cf8c3404d6a0382b3c16da30e11b6fe
GabrielGenevro <126488353+GabrielGenevro@users.noreply.github.com>
2026-06-01 09:31:56 -0300

    Create enviaremail.php
    codigo base para enviar email

---
+++ enviaremail.php
@@ -0,0 +1,49 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try 
+    {
+        // servidor SMTP
+        $mail->isSMTP();
+
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+
+        $mail->Username   = 'testesatc2@gmail.com';
+        $mail->Password   = 'senha';
+
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        // remetente
+        $mail->setFrom('testesatc2@gmail.com', 'Sistema de Bancas');
+
+        // destinatário
+        $mail->addAddress('gabrielgenevro01@gmail.com');
+
+        // conteúdo
+        $mail->isHTML(true);
+
+        $mail->Subject = 'Banca criada';
+
+        $mail->Body = '
+            <h1>Banca criada com sucesso</h1>
+            <p>Sua banca foi cadastrada.</p>
+        ';
+
+        $mail->send();
+
+        echo "Email enviado com sucesso";
+
+    } 
+catch (Exception $e) 
+    {
+        echo "Erro ao enviar email: {$mail->ErrorInfo}";
+    }
+?>
